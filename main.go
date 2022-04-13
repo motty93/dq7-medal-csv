aab8fc78818fe94dd6669cadb9949e48a060b525
motty93 <rdwbocungelt5@gmail.com>
2022-04-13 21:25:51 +0900

    feat: dq7のメダル有りかtableをscrapingしてcsvへ

--- main.go
+++ main.go
@@ -1,14 +1,20 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
 	"log"
+	"os"
+
+	"strings"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
 var (
-	records = [][]string{}
+	records = [][]string{
+		{"No", "Place", "Time", "Detail"},
+	}
 )
 
 func main() {
@@ -19,8 +25,35 @@ func main() {
 	}
 
 	doc.Find("table.table1").Children().Find("tr").Each(func(i int, s1 *goquery.Selection) {
+		record := make([]string, 4)
 		s1.Find("td").Each(func(j int, s2 *goquery.Selection) {
-			fmt.Println(j, s2.Text())
+			record[j] = strings.TrimSpace(s2.Text())
 		})
+		records = append(records, record)
 	})
+
+	// 出力するfile作成
+	file, err := os.Create("dq7_medal.csv")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	cw := csv.NewWriter(file)
+	defer cw.Flush()
+	// 標準出力する場合(確認用)
+	// w := csv.NewWriter(os.Stdout)
+
+	// 予めrecordsが2次元配列の形であればWriteAllでおｋ
+	cw.WriteAll(records)
+	// for _, record := range records {
+	// 	if err := w.Write(record); err != nil {
+	// 		log.Fatalln("error writing record to csv:", err)
+	// 	}
+	// }
+
+	if err := cw.Error(); err != nil {
+		log.Fatalln("error writing csv:", err)
+	}
+
+	fmt.Println("csv read ok")
 }
